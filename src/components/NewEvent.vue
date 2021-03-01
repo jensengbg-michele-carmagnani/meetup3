6e5c44d62fb8834f4c53bee855ab95119063514b
Michele Carmagnani <michele.carmagnani@gmail.com>
2021-03-01 21:39:45 +0100

    add form for new event

--- src/components/NewEvent.vue
+++ src/components/NewEvent.vue
@@ -1,42 +1,44 @@
 <template>
-<section id="new-event">
-
+  <section id="new-event">
     <article class="main">
       <p class="sign" align="center">Create Event</p>
       <form class="form1">
         <input
-        
           v-model="newEvent.name"
           class="name"
           type="text"
           align="center"
-          placeholder="Event's name " :class="{ 'fail-username': this.isFailed }"
+          placeholder="Event's name "
+          :class="{ 'fail-username': this.isFailed }"
         />
         <input
           v-model="newEvent.data"
           class="data"
           type="text"
           align="center"
-          placeholder=" xx/xx/xx/" :class="{ 'fail-password': this.isFailed }"
+          placeholder=" xx/xx/xx/"
+          :class="{ 'fail-password': this.isFailed }"
         />
         <input
           v-model="newEvent.timeIn"
           class="time"
           type="text"
           align="center"
-          placeholder="starting time 00:00" :class="{ 'fail-password': this.isFailed }"
+          placeholder="starting time 00:00"
+          :class="{ 'fail-password': this.isFailed }"
         />
         <textarea
-        rows="6"
-        cols="50"
+          rows="6"
+          cols="50"
           v-model="newEvent.description"
           class="description "
           type="text"
           align="center"
-          placeholder="Description " :class="{ 'fail-password': this.isFailed }"
+          placeholder="Description "
+          :class="{ 'fail-password': this.isFailed }"
         />
 
-        <button class="submit">Create Event</button>
+        <button @click="CreateEvent" class="submit">Create Event</button>
       </form>
     </article>
   </section>
@@ -47,21 +49,26 @@ export default {
   name: "newEvent",
   data() {
     return {
+      counter : 9,
       newEvent: {
-        idEvent: Math.floor(Math.random() * (10 - 1 + 1)) + 10,
+        idEvent: this.counter++,
         name: "",
+        location: "",
         date: "",
-        timeIn: "",
+        starting: "",
+        methods: "onLine",
         description: "",
+        image: "https://picsum.photos/200/300.webp",
+        review: [],
+        enrolled: [],
       },
     };
   },
-  methods:{
-    create(){
-
-      this.$store.dispatch('create', this.newEvent)
-    }
-  }
+  methods: {
+    CreateEvent() {
+      this.$store.dispatch("create", this.newEvent);
+    },
+  },
 };
 </script>
 
