e721b07e502f2a724eae5deab3ce94eb70fff9e1
Michele Carmagnani <michele.carmagnani@gmail.com>
2021-03-01 16:26:36 +0100

    create pannel for new event not the logic

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/login">Login</router-link>
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/addevent">Create event</router-link>
     </div>
     <router-view />
   </div>
---
+++ src/views/AddEvent.vue
@@ -0,0 +1,24 @@
+<template>
+  <section id="add-event">
+    <NewEvent/>
+  </section>
+</template>
+
+<script>
+import NewEvent from "@/components/NewEvent.vue"
+export default {
+name: "AddEvent",
+components:{
+  NewEvent
+},
+data() {
+  return {
+    
+  }
+},
+}
+</script>
+
+<style>
+
+</style>
